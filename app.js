82ed481054525c31b3a5630d243e5e61294b7c51
TimothieMT <ttolk89@gmail.com>
2022-07-19 14:57:57 +0200

    1.1

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-let credits = 100
+let credits = 200
 let displayCredits = document.querySelector(".credits")
-const displayResult = document.querySelector(".result")
+const displayResultOne = document.querySelector(".resultOne")
+const displayResultTwo = document.querySelector(".resultTwo")
+const displayResultThree = document.querySelector(".resultThree")
+
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+  
 
 function roll() {
-    
-    const dice = Math.round(Math.random() * 6)
 
-   
+
+    let i = 6;
+    let face = Array.from({length: i}, (_, i) => i + 1);
+    for(i=1;i< 6;i++) {
+    face[i] = new Image();
+    face[i].src="./dice-" + (i+1) + "-solid.svg";
+    }
+
+    let a = getRandom(1, 6)
+    let b = getRandom(1, 6)
+    let c = getRandom(1, 6)
     
-    if (dice == 6) {
-        credits = credits + 5
-        displayCredits.innerHTML = credits
-        displayResult.innerHTML = dice
+    const cubepicOne = document.querySelector(".resultOne").src=eval("face["+a+"].src")
+    const cubepicTwo = document.querySelector(".resultTwo").src=eval("face["+b+"].src")
+    const cubepicThree = document.querySelector(".resultThree").src=eval("face["+c+"].src")
+ 
+    if (a == 3 && b == 3 && c == 3 ) {
+        credits = credits + 200
+        displayCredits.innerHTML = credits + " €"
+        displayResultOne.innerHTML = cubepicOne
+        displayResultTwo.innerHTML = cubepicTwo
+        displayResultThree.innerHTML = cubepicThree
     } else {
         credits = credits - 1
-        displayCredits.innerHTML = credits
-        displayResult.innerHTML = dice
+        displayCredits.innerHTML = credits + " €"
+        
     }
 
     return credits
@@ -23,7 +48,12 @@ function roll() {
 
 function resetValue() {
     
-    displayCredits.innerHTML = 100
+    displayCredits.innerHTML = 200 + " €"
+   
     
     return displayCredits
 }
+
+
+
+
